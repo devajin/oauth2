003b032459a37f89d59f66e2bcd2230b9925b0cd
deva-jin <deva.jin@kakaocorp.com>
2020-11-18 11:37:36 +0900

    Initial commit

---
+++ src/main/java/com/deva/spring/oauth2/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+/**
+ * project name : oauth2
+ * Created by deva.jin on 2020/11/17.
+ */
+
+
+package com.deva.spring.oauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter {
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests();
+
+	}
+}
